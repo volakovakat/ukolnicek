02052803028446fbbc0027a266d4bd6390a4ab95
Katerina Volakova <volakova.kat@gmail.com>
2022-10-29 18:44:23 +0200

    řešení - aplikace zobrazující seznam úkolů

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log('funguju!');
+export const Task = (props) => {
+  const { task } = props; 
+
+  // HTML struktura s props
+  return `
+    <div class="task">
+      <div class="task__body">
+        <div class="task__name">${task.name}</div>
+        <div class="task__due">${task.due}</div>
+      </div>
+      <div class="task__done">${task.done? "✓" : "✗"}</div>
+    </div>
+    `
+}
+
+const fetchTasks = () => {
+  fetch('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks')
+  .then((response) => response.json())
+  .then((data) => {
+    renderTasks(data)
+  })
+}
+
+  const checkbox = document.querySelector("#checkbox-undone");
+  checkbox.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      fetch(`https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks?done=${!e.target.checked}`)
+      .then((response) => response.json())
+      .then((data) => {
+      renderTasks(data)
+  })
+    } else {
+      fetchTasks();
+    }
+  } )
+   
+// vyrenderuje tasky z API
+const renderTasks = (data) => {
+  const tasksElm = document.querySelector('.todo__tasks');
+  tasksElm.innerHTML = data.map(t => Task({task: t})).join('')
+  };
+
+  fetchTasks();
+
+  
+   
